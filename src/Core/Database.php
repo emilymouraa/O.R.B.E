a289c4d272b6b46ceaa8895ae8774c5031b40184
Daniel G. <danielgoncalvesribeiro14@gmail.com>
2026-03-02 21:20:53 -0300

    Conexão com o banco de dados

---
+++ src/Core/Database.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private static ?PDO $instance = null;
+
+    public static function getConnection(): PDO {
+        if (self::$instance === null) {
+            try {
+                self::$instance = new PDO("mysql:host=localhost;dbname=orbe;charset=utf8mb4", "root", 'csDk@2k24$Ç12');
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Erro na conexão: " . $e->getMessage());
+            }
+        }
+        return self::$instance;
+    }
+}
